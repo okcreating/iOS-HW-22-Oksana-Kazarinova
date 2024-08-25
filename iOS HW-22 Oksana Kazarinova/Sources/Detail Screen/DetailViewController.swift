c4a747f67213c0350bb0b08ff0d77be250a6a3ec
okcreating <oksibearus@gmail.com>
2024-08-25 20:14:27 +0100

    views and layouts

--- iOS HW-22 Oksana Kazarinova/Sources/Detail Screen/DetailViewController.swift
+++ iOS HW-22 Oksana Kazarinova/Sources/Detail Screen/DetailViewController.swift
@@ -9,12 +9,163 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
+    enum GenderPickerOptions: String {
+        case man = "Gentleman"
+        case woman = "Lady"
+        case notSure = "Other"
+    }
+
+    // MARK: - Outlets
+
+    lazy var avatarContainer: UIImageView = {
+        let avatarContainer = UIImageView()
+        avatarContainer.contentMode = .scaleToFill
+        avatarContainer.layer.masksToBounds = true
+        avatarContainer.clipsToBounds = true
+        avatarContainer.layer.cornerRadius = 12
+        return avatarContainer
+    }()
+
+    lazy var nameIconContainer: UIImageView = {
+        let imageContainer = UIImageView()
+        imageContainer.contentMode = .scaleToFill
+        imageContainer.layer.masksToBounds = true
+        imageContainer.clipsToBounds = true
+        //imageContainer.tintColor = .white
+        return imageContainer
+    }()
+
+    lazy var nameLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 13, weight: .regular)
+        label.textAlignment = .left
+        return label
+    }()
+
+    lazy var dateIconContainer: UIImageView = {
+        let imageContainer = UIImageView()
+        imageContainer.contentMode = .scaleToFill
+        imageContainer.layer.masksToBounds = true
+        imageContainer.clipsToBounds = true
+        //imageContainer.tintColor = .white
+        return imageContainer
+    }()
+
+    lazy var dateOfBirthLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 13, weight: .regular)
+        label.textAlignment = .left
+        return label
+    }()
+
+    lazy var genderIconContainer: UIImageView = {
+        let imageContainer = UIImageView()
+        imageContainer.contentMode = .scaleToFill
+        imageContainer.layer.masksToBounds = true
+        imageContainer.clipsToBounds = true
+      //  imageContainer.tintColor = .white
+        return imageContainer
+    }()
+
+    lazy var genderLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 13, weight: .regular)
+        label.textAlignment = .left
+        label.text = "Gender"
+        return label
+    }()
+
+    lazy var genderPicker: UIPickerView = {
+        let picker = UIPickerView()
+        return picker
+    }()
+
+    lazy var nameStack: UIStackView = {
+        let view = UIStackView()
+        view.axis = .horizontal
+        return view
+    }()
+
+    lazy var dateStack: UIStackView = {
+        let view = UIStackView()
+        view.axis = .horizontal
+        return view
+    }()
+
+    lazy var genderStack: UIStackView = {
+        let view = UIStackView()
+        view.axis = .horizontal
+        return view
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupHierarchy()
+        setupLayout()
+        setupNavigationBar()
     }
-    
+
+    func setupHierarchy() {
+        view.addSubview(avatarContainer)
+        nameStack.addSubview(nameIconContainer)
+        nameStack.addSubview(nameLabel)
+        dateStack.addSubview(dateIconContainer)
+        dateStack.addSubview(dateOfBirthLabel)
+        genderStack.addSubview(genderIconContainer)
+        genderStack.addSubview(genderLabel)
+        genderStack.addSubview(genderPicker)
+        view.addSubview(nameStack)
+        view.addSubview(dateStack)
+        view.addSubview(genderStack)
+    }
+
+    func setupLayout() {
+        avatarContainer.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(25)
+            make.width.height.equalTo(50)
+        }
+        nameIconContainer.snp.makeConstraints { make in
+            make.leading.top.bottom.equalTo(nameStack).offset(5)
+        }
+        nameLabel.snp.makeConstraints { make in
+            make.leading.equalTo(nameIconContainer.snp.trailing).offset(10)
+            make.top.bottom.equalTo(nameStack).offset(5)
+        }
+        dateIconContainer.snp.makeConstraints { make in
+            make.leading.top.bottom.equalTo(dateStack).offset(5)
+        }
+        dateOfBirthLabel.snp.makeConstraints { make in
+            make.leading.equalTo(dateIconContainer.snp.trailing).offset(10)
+            make.top.bottom.equalTo(dateStack).offset(5)
+        }
+        genderIconContainer.snp.makeConstraints { make in
+            make.leading.top.bottom.equalTo(genderStack).offset(5)
+        }
+        genderLabel.snp.makeConstraints { make in
+            make.leading.equalTo(genderIconContainer).offset(10)
+            make.top.bottom.equalTo(genderStack).offset(5)
+        }
+        genderPicker.snp.makeConstraints { make in
+            make.trailing.top.bottom.equalTo(genderStack).offset(5)
+        }
+        nameStack.snp.makeConstraints { make in
+            make.top.equalTo(avatarContainer.snp.bottom).offset(20)
+            make.leading.trailing.equalTo(view).offset(5)
+        }
+        dateStack.snp.makeConstraints { make in
+            make.top.equalTo(nameStack.snp.bottom).offset(10)
+            make.leading.trailing.equalTo(view).offset(5)
+        }
+        genderStack.snp.makeConstraints { make in
+            make.top.equalTo(dateStack.snp.bottom).offset(20)
+            make.leading.trailing.equalTo(view).offset(5)
+        }
+    }
+
+    func setupNavigationBar() {
+
+    }
+
 
 
 
--- iOS HW-22 Oksana Kazarinova/Sources/Main Screen/MainTableViewCell.swift
+++ iOS HW-22 Oksana Kazarinova/Sources/Main Screen/MainTableViewCell.swift
@@ -6,9 +6,40 @@
 //
 
 import UIKit
+import SnapKit
 
 class MainTableViewCell: UITableViewCell {
 
     static let identifier = "MainTableViewCell"
 
+    // MARK: - Outlets
+
+    private lazy var name: UILabel = {
+        let name = UILabel()
+        name.font = UIFont.systemFont(ofSize: 13, weight: .regular)
+        return name
+    }()
+
+    // MARK: - Initializers
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: MainTableViewCell.identifier)
+        selectionStyle = .blue
+        setupHierarchy()
+        setupLayout()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func setupHierarchy() {
+        addSubview(name)
+    }
+
+    func setupLayout() {
+        name.snp.makeConstraints { make in
+            make.leading.top.bottom.equalTo(contentView).offset(7)
+        }
+    }
 }
